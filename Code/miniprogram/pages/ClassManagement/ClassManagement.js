ce0738de37f83c5f47c521625fd31bb12b4e4a61
Wanshe <1124864321@qq.com>
2021-05-22 10:29:42 +0800

    新建类管理页面 修改Form页日历逻辑
    新建类管理ClassManagement页面
    新建类条ClassBar组件
    修改Form页日历逻辑使得不能选中超过3个天数

---
+++ Code/miniprogram/pages/ClassManagement/ClassManagement.js
@@ -0,0 +1,79 @@
+// miniprogram/pages/ClassManagement/ClassManagement.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    classGroup:[
+      {
+        name:'团支部'
+      },
+      {
+        name:'求是潮'
+      },
+      {
+        name:'思修小组'
+      },
+      {
+        name:'沟通技巧'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
